a6a164e849d20700747540f3dcf9e87629b0c7ec
trpmoreira <73557239+trpmoreira@users.noreply.github.com>
2023-07-09 22:24:11 +0100

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p>AdminCenter</p>
+      <Button>Button</Button>
+    </div>
+  );
+}
