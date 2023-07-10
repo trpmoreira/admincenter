4a296112e7116c912b540e0d0d4b32e6d3d05105
trpmoreira <73557239+trpmoreira@users.noreply.github.com>
2023-07-11 00:40:15 +0100

    Navigation Menu

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,18 @@
-const DashboardPage = () => {
-  return <div>This a dashboard</div>;
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findUnique({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return <div>Active store: {store?.name}</div>;
 };
 
 export default DashboardPage;
